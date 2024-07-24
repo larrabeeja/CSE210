034a8fdcfbc15dfc76ba073f018cee65a793ffce
larrabeeja <larrabee.jason@gmail.com>
2024-07-24 00:45:29 -0400

    why did this not save?

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,124 @@
 using System;
+using System.Collections.Generic;
+
+public abstract class Activity
+{
+    private DateTime date;
+    private int minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public DateTime Date => date;
+    public int Minutes => minutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{date.ToShortDateString()} {this.GetType().Name} ({minutes} min) - Distance: {GetDistance():0.0} miles, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+    }
+}
+
+public class Running : Activity
+{
+    private double distance;
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (distance / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / distance;
+    }
+}
+
+public class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (speed * Minutes) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+}
+
+public class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50 / 1000.0 * 0.62;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Create Activity instances
+        var running = new Running(new DateTime(2024, 11, 3), 30, 3.0); // 3 miles in 30 minutes
+        var cycling = new Cycling(new DateTime(2024, 11, 3), 30, 20.0); // 20 mph for 30 minutes
+        var swimming = new Swimming(new DateTime(2024, 11, 3), 30, 40); // 40 laps in 30 minutes
+
+        // Create a list of activities
+        var activities = new List<Activity> { running, cycling, swimming };
+
+        // Iterate through the list of activities and display summaries
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
